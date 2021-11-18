5c3d365d15b49aa1fea19ef8eae6bb1c43e2be44
shin <tlsrhkdcjs12@naver.com>
2021-11-18 19:01:23 +0900

    Django Study Start

--- Django_Tutorial/foods/views.py
+++ Django_Tutorial/foods/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("<h2>Hello, Django</h2>")
+
+def hello_view(request):
+    return HttpResponse('<h1>Hola! :)</h1>')
