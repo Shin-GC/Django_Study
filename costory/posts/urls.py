966bae6c4177fe1e5a289420a2313626ca5780d2
shin <tlsrhkdcjs12@naver.com>
2021-12-07 17:32:00 +0900

    [템플릿 필터 및 태그 학습] 12-07 도서관

--- costory/posts/urls.py
+++ costory/posts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns =[
 #     path('', views.index),
-#     path('posts/', views.post_list),
+    path('posts/', views.post_list, name='post-list'),
 #     path('post/new', views.post_create),
-#     path('post/<int:post_id>/', views.post_detail),
-#     path('post/<int:post_id>/eidt/', views.post_upadte),
-#     path('post/<int:post_id>/delete/', views.post.delete),
+    path('posts/<int:post_id>/', views.post_detail, name='post-detail'),
+#     path('posts/<int:post_id>/eidt/', views.post_upadte),
+#     path('posts/<int:post_id>/delete/', views.post.delete),
 ]
--- mindnote/mindnote/diary/views.py
+++ mindnote/mindnote/diary/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Page
+
 
 # Create your views here.
+
+def page_list(request):
+    pages = Page.objects.all()
+    context = {'object_list': pages}
+    return render(request, 'diary/page_list.html', context=context)
+
+
+def page_detail(request, page_id):
+    page = Page.objects.get(id=page_id)
+    context = {'object': page}
+    return render(request, 'diary/page_detail.html', context=context)
