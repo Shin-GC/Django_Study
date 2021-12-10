31c21fbde759c99fc03b2edd05a2b2d668ae9865
shin <tlsrhkdcjs12@naver.com>
2021-12-10 17:26:35 +0900

    [마인드노트] forms.py에 widgets 구현해보기

--- mindnote/mindnote/diary/forms.py
+++ mindnote/mindnote/diary/forms.py
@@ -6,3 +6,21 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(
+                attrs={'placeholder': '제목을 입력해주세요'}
+            ),
+            'dt_created': forms.DateTimeInput(
+                attrs={'placeholder': '날짜를 입력해주세요'}
+            ),
+            'score': forms.NumberInput(
+                attrs={'placeholder': '숫자만 입력해주세요.'}
+            ),
+            'feeling': forms.TextInput(
+                attrs={'placeholder': '상태를 입력해주세요'}
+            ),
+            'content': forms.Textarea(
+                attrs={'placeholder': '내용을 입력해주세요! (최소 10자 이상!)'}
+            )
+        }
+
