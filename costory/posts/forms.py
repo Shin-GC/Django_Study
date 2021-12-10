0dd0fab4a4192e28fab450cfb9a7b48fd8165f2e
shin <tlsrhkdcjs12@naver.com>
2021-12-10 17:15:18 +0900

    [마인드노트] static 폴더 및 form 디자인 추가

--- costory/posts/forms.py
+++ costory/posts/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Post
+from .validators import validate_symbols
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ['title', 'content']
+        widgets = {'title': forms.TextInput(attrs={'class': 'title',
+                                                   'placeholder': '제목을 입력 하세요.'}),
+                   'content': forms.Textarea(attrs={'class': 'content',
+                                                    'placeholder': '내용을 입력 하세요.'})}
