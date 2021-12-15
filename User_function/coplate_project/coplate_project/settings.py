8dfa97dc0f99c35b959c70edc7e6dfd6ddbd399e
SGC <tlsrhkdcjs12@naver.com>
2021-12-15 19:13:04 +0900

    [Django SecretKey] 장고 SecretKey 변경 및 숨기기

--- User_function/coplate_project/coplate_project/settings.py
+++ User_function/coplate_project/coplate_project/settings.py
@@ -10,18 +10,36 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import json
+import os
 from pathlib import Path
 
+from django.core.exceptions import ImproperlyConfigured
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4s@b9inl&krcvzm5m!+ex0ckx(r)33mcs@^%+&=@ef@6-j2()%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
--- User_function/podomarket_project/podomarket_project/settings.py
+++ User_function/podomarket_project/podomarket_project/settings.py
@@ -11,17 +11,30 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import json
+import os
+from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5$x37ts@xfgk@21j0a)a7-s1%ioqf)68tyen0o@w@38$dms-m!'
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
